4b46be7a0b2df6743095b668935b75e78c40dc50
s.n.sanjay <sanjaysaravanakumar31@gmail.com>
2024-07-23 13:21:55 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Login from './components/Login'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Login />
+
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,10 @@
+const Login = () => {
+    return ( 
+        <div className="bg-[#f5f5f5] h-[100vh]  ">
+        
+
+        </div>
+     );
+}
+ 
+export default Login;
